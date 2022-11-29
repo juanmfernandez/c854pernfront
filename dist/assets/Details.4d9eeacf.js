0d24b8d9f66fb74449c393895eede03ee0c1fba0
Juan <juanm.fernandez@hotmail.com>
2022-11-29 15:39:41 -0300

    Build5

---
+++ dist/assets/Details.4d9eeacf.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index.5f2b1954.js";const a=s=>r("div",{children:"Details"});export{a as default};
---
+++ dist/assets/EditAndDelete.06ec9463.js
@@ -0,0 +1,1 @@
+import{N as r}from"./NavbarAdmin.886271b4.js";import{j as s,a}from"./index.5f2b1954.js";const t=()=>s("div",{children:[a(r,{}),a("h1",{children:"asdasd"})]});export{t as default};
