574345aebaa9211937ffc08228cb8f27a3f30120
Juan <juanm.fernandez@hotmail.com>
2022-11-29 00:35:27 -0300

    Build

---
+++ dist/assets/Admin.9f8ef485.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index.deccb83f.js";const a=r=>n("div",{children:"Admin Panel"});export{a as default};
---
+++ dist/assets/Details.15e18233.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.deccb83f.js";const e=s=>r("div",{children:"Details"});export{e as default};
